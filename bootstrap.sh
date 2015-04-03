90e91a37650786a29c670a1d2baca063c9c7de3a
miyase <xxtakuxx@biriken.jp>
2015-04-03 16:39:32 +0900

    cakephpのバージョンを2.6に指定

--- bootstrap.sh
+++ bootstrap.sh
@@ -71,7 +71,7 @@ mv composer.phar /usr/local/bin/composer
 echo "-------------------------------"
 echo "install cakephp"
 echo "-------------------------------"
-git clone git://github.com/cakephp/cakephp.git /vagrant/www/cake-app
+git clone -b 2.6 git://github.com/cakephp/cakephp.git /vagrant/www/cake-app
 git clone git://github.com/cakephp/debug_kit.git /vagrant/www/cake-app/Plugins/DebugKit
 
 mysql -u root -proot -e "CREATE DATABASE cake_app;"
